e1af886ea1b7e27199c53fc5600e450bf53add9f
Maksym Fedorets <ltif427@gmail.com>
2022-11-18 22:47:35 +0100

    Added structure Variables and comments

--- Zadanie4(WDP).cpp
+++ Zadanie4(WDP).cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-int x = 10, y = 5, out, *px = &x, *px2 = &y;
-char operation;
+struct Variables {
+    int x = 10, y = 5, out{}, * px = &x, * px2 = &y;
+    int* arr = new int[x];
+};
 
 /// <summary>
 /// Metoda, która generuje proste operacje matematyczne
 /// </summary>
 /// <param name="operation">rodzaj operacji, która ma być wykonana</param>
 /// <returns>result</returns>
-void GetOperation(char operation) {
+void GetOperation(int *num1, int *num2, int result, char operation) {
     switch (operation) {
     case '+':
-        printf("suma: %d + %d = %d\n", *px, *px2, out = *px + *px2); break;
+        printf("suma: %d + %d = %d\n\n", *num1, *num2, result = *num1 + *num2); break;
     case '*':
-        printf("iloczyn: %d * %d = %d\n", *px, *px2, out = *px * *px2); break;
+        printf("iloczyn: %d * %d = %d\n\n", *num1, *num2, result = *num1 * *num2); break;
     case '/':
-        printf("iloraz: %d / %d = %d\n", *px, *px2, out = *px / *px2); break;
+        printf("iloraz: %d / %d = %d\n\n", *num1, *num2, result = *num1 / *num2); break;
     case '-':
-        printf("roznica: %d - %d = %d\n", *px, *px2, out = *px - *px2); break;
+        printf("roznica: %d - %d = %d\n\n", *num1, *num2, result = *num1 - *num2); break;
     default:
         cout << "Nieznana operacja\n\n"; break;
     }
@@ -27,13 +29,34 @@ void GetOperation(char operation) {
 
 int main()
 {
-    cout << "px:\t" << *px << endl;
-    cout << "px2:\t" << *px2 << endl;
+    // tworzymy strukturę vars1
+    Variables vars1;
+
+    // wyświetlić w konsoli px i px2
+    cout << "px:\t" << *vars1.px << endl;
+    cout << "px2:\t" << *vars1.px2 << endl;
+
+    // wypełnianie tablicy danymi
+    for (int i = 0; i < vars1.x; i++)
+        vars1.arr[i] = i + 1;
 
     // wykonać wybraną przez użytkownika operację na px i px2
+    char operation;
+
     cout << "Podaj operacje (-, +, /, *): ";
     cin >> operation;
-    GetOperation(operation);
+
+    GetOperation(vars1.px, vars1.px2, vars1.out, operation);
+
+    cout << "adres zmiennej, ktora zawierajaca odpowiedz: " << &vars1.out << endl;
+
+    // wyprowadzić tablicę
+    cout << "Tablica 'arr': ";
+    for (int i = 0; i < vars1.x; i++)
+        cout << vars1.arr[i] << " ";
+
+    // usunąńć tavlicę
+    delete[] vars1.arr;
 
     return 0;
 }
